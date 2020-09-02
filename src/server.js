eb27fb87e9d4040ecfcb2c82c7c2529276806c2a
root <hamzaali151214@hotmail.com>
2020-09-02 20:49:10 +0000

    set up a page

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 5555;
-const host = '127.0.0.1';
+const host = "127.0.0.1";
 
-app.get("/",function(req,res){
-	res.sendFile(path.join(__dirname,"index.html"));
-})
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(port);
-console.log(`Listening on port ${port}`)
+console.log(`Listening on port ${port}`);
