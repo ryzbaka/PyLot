aedb2dbca9543b6a06f5fee408e19751c0aaf735
Hamza Ali Rizvi <hamzaali151214@hotmail.com>
2020-11-05 13:39:46 +0530

    completed react terminal implementation and tested with wsl

--- frontend/src/Terminal.jsx
+++ frontend/src/Terminal.jsx
@@ -3,11 +3,6 @@ import React, {Component,useContext} from "react";
 import {XTerm} from 'xterm-for-react';
 import openConnection from "socket.io-client";
 import SignInContext from "./SignInContext";
-// function subscribeToSocket(ipAddr, callBack) {
-//   const socket = openConnection(`http://${ipAddr}:5000/`);
-//   socket.emit("get-health", window.location.href);
-//   socket.on("send-health", (receivedData) => callBack(null, receivedData));
-// }
 class Terminal extends Component{
     constructor(props){
         super(props);
@@ -19,7 +14,24 @@ class Terminal extends Component{
     static contextType=SignInContext;//the sign in context can now be accessed using this.context
     componentDidMount(){
         this.xtermRef.current.terminal.writeln("Welcome to PyLot terminal");
-        
+        const socket = openConnection("http://localhost:5550/");
+        this.state.socket = socket;
+        // console.log(this.state);
+        // this.xtermRef.current.terminal.writeln('\x1B[1;1;32m***Connected to Socket Server ✔ ***\x1B[0m \n\r')
+        socket.on('connect',()=>{
+            this.writeToTerminal('\x1B[1;1;32m***Connected to PyLot terminal runtime ✈ ***\x1B[0m \n\r')
+        })
+        socket.on('disconnect',()=>{
+            this.writeToTerminal('\x1B[1;3;31m***Disconnected from PyLot terminal runtime ☠ ***\x1B[0m \n\r')
+        })
+        socket.on('send-output',data=>{
+            this.xtermRef.current.terminal.write(data)
+            console.log(data.split(';'))
+            console.log(data)
+        })
+    }
+    writeToTerminal(dataString){
+        this.xtermRef.current.terminal.writeln('\n\r'+dataString)
     }
     render(){
         
@@ -28,9 +40,9 @@ class Terminal extends Component{
                 <XTerm ref={this.xtermRef} onKey={({key,domEvent})=>{
                     
                         if(domEvent.code==="Enter"){
-                            console.log("enter key was pressed.")
-                            console.log(`The command issued was ${this.state.command}`)
-                            this.state.command=[];
+                            this.xtermRef.current.terminal.writeln("");
+                            this.state.socket.emit('run-command',this.state.command);
+                            this.state.command="";
                         }else{
                             this.state.command+=key
                             this.xtermRef.current.terminal.write(key)
