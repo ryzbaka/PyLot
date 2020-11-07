91ef3c86652eb48b46f0ceb3411be6eec79101e9
root <hamzaali151214@hotmail.com>
2020-11-07 11:36:15 +0000

    build 2 is the old build without terminal functionality

---
+++ frontend/build2/precache-manifest.ca11cbf45617a4a180e7728b6cef2723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65387c60f7c1914ee22254e66ec76ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2e38fe0d5dc4263aa5a",
+    "url": "/static/css/main.bcb2d790.chunk.css"
+  },
+  {
+    "revision": "24c2e000ef019056dfe0",
+    "url": "/static/js/2.f20113b5.chunk.js"
+  },
+  {
+    "revision": "65bb8e8d9e0b75c5106d98cecad3e095",
+    "url": "/static/js/2.f20113b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e38fe0d5dc4263aa5a",
+    "url": "/static/js/main.bdb4a1b5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
